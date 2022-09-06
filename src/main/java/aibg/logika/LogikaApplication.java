dc2197b518c1cfa44b304fbdc894e7f20548aa90
AleksandarGazikalovic <93039913+AleksandarGazikalovic@users.noreply.github.com>
2022-09-06 17:14:39 +0200

    Initial commit

---
+++ src/main/java/aibg/logika/LogikaApplication.java
@@ -0,0 +1,13 @@
+package aibg.logika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aibg/logika/LogikaApplicationTests.java
@@ -0,0 +1,13 @@
+package aibg.logika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
